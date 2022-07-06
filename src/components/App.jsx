d08d1c7dbab5fa0e5e97f945557cea8747b04f06
barbararech <rech.brb@gmail.com>
2022-07-06 10:55:35 -0300

    add fonts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import styled from 'styled-components';
 import Login from './Login';
 import SignUp from './SignUp';
-import styled from 'styled-components';
+import Success from "./Success";
 
 export default function App() {
     return (
@@ -9,7 +10,14 @@ export default function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Login />} />
-                    <Route path="/sign" element={<SignUp />} />
+                    <Route path="/signup" element={<SignUp />} />
+                    {/* <Route path="/profile" element={<SignUp />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/game/id" element={<Game />} />
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="/checkout" element={<Checkout />} />
+                    <Route path="/orders" element={<Orders />} /> */}
+                    <Route path="/success" element={<Success />} />
                 </Routes>
             </BrowserRouter>
         </Container>
---
+++ src/components/Success.jsx
@@ -0,0 +1,8 @@
+export default function Success() {
+    
+    return (
+        <>
+            LOGIN AQUI
+        </>
+    )
+}
