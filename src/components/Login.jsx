b8043620ee2a0c7609e693e010201e2eebba8cbf
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2022-07-07 16:08:20 -0300

    register participants

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,19 +1,38 @@
 import styled from 'styled-components';
-import {Link} from 'react-router-dom';
-import {useState} from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import Logo from '../assets/neon-control.webp';
+import axios from 'axios';
 
 export default function Login() {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    function signIn() {
+        const user = {
+            email: email,
+            password: password
+        }
+        console.log(user);
+        const promisse = axios.post('localhost:5001', user);
+        promisse.then(response => {
+            console.log(response);
+            navigate('/profile');
+        })
+            .catch(erro => { alert('bad request') });
+    }
 
     return (
         <Align>
             <div>
-                <div><img src="" alt="neon-games" /></div>
-                <input placeholder=' email' type='email' value={email}/>
-                <input placeholder=' senha' type='password' value={password}/>
-                <button>Entrar</button>
+                <h1>NEON GAME</h1>
+                <h1>STORE</h1>
+                <div><img src={Logo} alt="neon-games" /></div>
+                <input placeholder=' email' type='email' value={email} onChange={e => setEmail(e.target.value)} />
+                <input placeholder=' senha' type='password' value={password} onChange={e => setPassword(e.target.value)} />
+                <button onClick={signIn}>Entrar</button>
                 <Link to='/sign-up'><p>Não tem uma conta? Cadastre-se</p></Link>
             </div>
         </Align>
@@ -25,17 +44,22 @@ const Align = styled.section`
     display: flex;
     justify-content: center;
     align-items: center;
-    background-color: #aeff17;
+    background-color: #000000;
     text-align: center;
+    font-family: 'Goldman';
+    font-style: normal;
+    font-weight: 400;
+    color: white;
     img{
-        margin-bottom: 25px;
+        width: 303px;
+        border-radius: 50%;
     }
     input{
         display: block;
         width: 303px;
         height: 50px;
         border-radius: 5px;
-        border: none;
+        border: 2px groove #F317FF;
         margin-bottom: 15px;
     }
     button{
@@ -44,5 +68,6 @@ const Align = styled.section`
         border-radius: 5px;
         border: none;
         margin-bottom: 15px;
+        border: 4px groove #F317FF;
     }
 `;
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,21 +1,48 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styled from 'styled-components';
 import { useState } from "react";
+import Logo from '../assets/neon-control.webp';
+import axios from "axios";
 
 export default function SignUp(){
 
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [name, setName] = useState('');
+    const [confirmPassword , setConfirm] = useState('');
+    const navigate = useNavigate();
+
+    function register(){
+        const newUser = {
+            email: email,
+            name: name,
+            password: password
+        }
+        if(password !== confirmPassword){
+            alert("CONFIRME SUA SENHA CORRETAMENTE");
+            return
+        }
+        const promisse = axios.post('localhost:5001/sign-up', newUser);
+        promisse.then(response =>{
+            console.log(response);
+            navigate('/');
+        })
+        .catch( error =>{
+            console.log(error);
+            alert('TRY AGAIN');
+        });
+    }
 
     return(
         <Align>
             <div>
-                <div><img src="" alt="neon-games" /></div>
-                <input placeholder=' nome' type='text'  value={name}/>
-                <input placeholder=' email' type='email'  value={email}/>
-                <input placeholder=' senha' type='password' value={password}/>
-                <button>Entrar</button>
+                <h1>Faça parte da nossa comunidade</h1>
+                <div><img src={Logo} alt="neon-games" /></div>
+                <input placeholder=' nome' type='text' value={name} onChange={e => setName(e.target.value)}/>
+                <input placeholder=' email' type='email' value={email} onChange={e => setEmail(e.target.value)}/>
+                <input placeholder=' senha' type='password' value={password} onChange={e => setPassword(e.target.value)}/>
+                <input placeholder=' confirme sua senha' type='password' value={confirmPassword} onChange={e => setConfirm(e.target.value)}/>
+                <button onClick={register}>Entrar</button>
                 <Link to='/'><p>Ja tem uma conta? Faça o login aqui</p></Link>
             </div>
         </Align>
@@ -27,17 +54,22 @@ const Align = styled.section`
     display: flex;
     justify-content: center;
     align-items: center;
-    background-color: #aeff17;
+    background-color: #000000;
     text-align: center;
+    font-family: 'Goldman';
+    font-style: normal;
+    font-weight: 400;
+    color: white;
     img{
-        margin-bottom: 25px;
+        width: 303px;
+        border-radius: 50%;
     }
     input{
         display: block;
         width: 303px;
         height: 50px;
         border-radius: 5px;
-        border: none;
+        border: 2px groove #F317FF;
         margin-bottom: 15px;
     }
     button{
@@ -46,5 +78,6 @@ const Align = styled.section`
         border-radius: 5px;
         border: none;
         margin-bottom: 15px;
+        border: 4px groove #F317FF;
     }
 `;
