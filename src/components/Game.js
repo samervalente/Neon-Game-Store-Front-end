b9c075e4c309aa899a74e1b4e0123c27f8d96d3d
barbararech <rech.brb@gmail.com>
2022-07-07 13:42:37 -0300

    feat: create user context

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,22 +1,19 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-export default function Game(){
-    const [game, setGame] = useState({})
-    const {id} = useParams()
-    useEffect(() => {
-        async function FetchData(){
-            const {data} = await axios.get(`https://neon-game-store-back.herokuapp.com/games/${id}`)
-            setGame(data)
-        }
-        FetchData()
-    },[])
+export default function Game() {
+  const [game, setGame] = useState({});
+  const { id } = useParams();
+  useEffect(() => {
+    async function FetchData() {
+      const { data } = await axios.get(
+        `https://neon-game-store-back.herokuapp.com/games/${id}`
+      );
+      setGame(data);
+    }
+    FetchData();
+  }, []);
 
-
-    return (
-        <p>Olá, eu sou o game {game.name}</p>
-    )
+  return <p>Olá, eu sou o game {game.name}</p>;
 }
-
-
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,59 +1,63 @@
-import {useState, useEffect} from "react"
-import { useNavigate} from "react-router-dom"
-import styled from "styled-components"
-import axios from "axios"
-import Footer from "../shared/Footer"
-import "../assets/styles/Home.css"
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import axios from "axios";
+import Footer from "../shared/Footer";
+import "../assets/styles/Home.css";
 
-export default function Home(){
-    const [games, setGames] = useState([])
-    const navigate = useNavigate()
+export default function Home() {
+  const [games, setGames] = useState([]);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        async function FetchData(){
-            const {data} = await axios.get("https://neon-game-store-back.herokuapp.com/games")
-            setGames(data)
-        }
-        FetchData()
-    }, [])
-
-    function choiceGame(id){
-        navigate(`/game/${id}`)
+  useEffect(() => {
+    async function FetchData() {
+      const { data } = await axios.get(
+        "https://neon-game-store-back.herokuapp.com/games"
+      );
+      setGames(data);
     }
+    FetchData();
+  }, []);
 
-    const gameComponent = games.map(game => {
-        return <div key={game._id} className="gameSection" onClick={() => choiceGame(game._id)}>
-            <img className="gameImage" src={game.imageURL}/>
-            <p className="name">{game.name}</p>
-            <p className="price">{game.price.toFixed(2)}</p>
-            <div className="addcart">Adicionar ao carrino</div>
-        </div> 
-    })
+  function choiceGame(id) {
+    navigate(`/game/${id}`);
+  }
 
+  const gameComponent = games.map((game) => {
     return (
-        <div className="Container Home">
-                <header className="TopBar">    
-                       <h1>Logo Em Construção</h1>
-                </header>
-               <Main>
-                    <ul className="Category">
-                            <li clicked="true">Popular</li>
-                            <li>Ação</li>
-                            <li>Aventura</li>
-                            <li>FPS</li>
-                    </ul>
-                    <div className="GamesSection">
-                            {gameComponent}
-                    </div>
-               </Main>
-               <Footer />
-        </div>
-    )
+      <div
+        key={game._id}
+        className="gameSection"
+        onClick={() => choiceGame(game._id)}
+      >
+        <img className="gameImage" src={game.imageURL} alt="imgGame" />
+        <p className="name">{game.name}</p>
+        <p className="price">{game.price.toFixed(2)}</p>
+        <div className="addcart">Adicionar ao carrino</div>
+      </div>
+    );
+  });
+
+  return (
+    <div className="Container Home">
+      <header className="TopBar">
+        <h1>Logo Em Construção</h1>
+      </header>
+      <Main>
+        <ul className="Category">
+          <li clicked="true">Popular</li>
+          <li>Ação</li>
+          <li>Aventura</li>
+          <li>FPS</li>
+        </ul>
+        <div className="GamesSection">{gameComponent}</div>
+      </Main>
+      <Footer />
+    </div>
+  );
 }
 
 const Main = styled.main`
-background-color: #151515;
-height:auto;
-
-
-`
+  background-color: #151515;
+  height: auto;
+`;
