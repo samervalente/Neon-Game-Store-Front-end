72eaa276c8f25d8ea260e1aaab30d3b6fc9a0c0a
samer <samervalente@gmail.com>
2022-07-08 16:19:53 -0300

    chore: add Neon button in shared fold

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,15 +1,7 @@
 import NeonButton from "../shared/NeonButton"
 
 
-export default function Profile(){
 
-    return (
-        <>
-            <p>Eu sou o Checkout</p>
-            <NeonButton content={"Confirmar"} />
-        </> 
-    )
-}
 
 
 
