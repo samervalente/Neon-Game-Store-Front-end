6c0e24d51484ed841e74a23e180b81b5119352e4
barbararech <rech.brb@gmail.com>
2022-07-08 15:05:29 -0300

    feat: add payment methods

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -4,18 +4,25 @@ import { useState, useEffect, useContext } from "react";
 import UserContext from "../context/UserContext";
 import axios from "axios";
 import styled from "styled-components";
-import { IoCloseCircleSharp, IoBagCheckOutline } from "react-icons/io5";
-import Collapsible from 'react-collapsible';
+import { IoBagCheckOutline } from "react-icons/io5";
+import Collapsible from "react-collapsible";
+import dayjs from "dayjs";
 import Footer from "../shared/Footer.js";
 
 export default function Checkout() {
   const navigate = useNavigate();
   const { user } = useContext(UserContext);
   const { name, token } = user;
-  const [productsCart, setProductsCart] = useState([]);
+  const [order, setOrder] = useState([]);
+  const { total, products } = order;
+  const [phone, setPhone] = useState([]);
+  const [address, setAddress] = useState([]);
+  const [cpf, setCpf] = useState([]);
+  const [payment, setPayment] = useState([]);
+  const date = dayjs().format("DD/MM/YYYY");
 
   useEffect(() => {
-    async function GetProductsCart() {
+    async function GetOrder() {
       const config = {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -24,32 +31,31 @@ export default function Checkout() {
 
       try {
         const { data } = await axios.get(
-          "https://neon-game-store-back.herokuapp.com/cart"
+          "https://neon-game-store-back.herokuapp.com/checkout"
         );
 
-        setProductsCart(data);
+        setOrder(data);
       } catch (error) {
         const message = error.response.statusText;
         alert(message);
       }
     }
-    GetProductsCart();
+    GetOrder();
   }, []);
 
   function RenderProducts() {
-    if (productsCart.length === 0) {
+    if (order.length === 0) {
       return (
         <div>
-          <p>
+          {/* <p>
             Não há produtos <br></br> em seu carrinho
-          </p>
+          </p> */}
         </div>
       );
     }
 
-    return productsCart.map((product, index) => {
+    return products.map((product, index) => {
       const { name, price, imageURL, _id } = product;
-
       return (
         <>
           <Product key={index}>
@@ -61,9 +67,6 @@ export default function Checkout() {
             </Info>
             <Info>
               <span>${price} </span>
-              <i onClick={() => Delete(_id)}>
-                <IoCloseCircleSharp />
-              </i>
             </Info>
           </Product>
         </>
@@ -71,59 +74,83 @@ export default function Checkout() {
     });
   }
 
-  function CalculateTotal() {
-    const initialValue = 0;
-
-    return productsCart.reduce((previousValue, currentValue) => {
-      return previousValue + currentValue.price;
-    }, initialValue);
-  }
-
   function RenderTotal() {
-    if (productsCart.length > 0) {
-      const total = CalculateTotal().toFixed(2);
-      return (
-        <>
-          <Total total={total}>
-            <span>SALDO</span>
-            <span>${total}</span>
-          </Total>
-          <button onClick={() => SubmitCheckout(total)}>Finalizar compra</button>
-        </>
-      );
-    }
-  }
-
-  async function Delete(_id) {
-    const config = {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    };
-
-    let confirmAlert = window.confirm(
-      "Você tem certeza que quer remover esse produto do carrinho?"
+    return (
+      <>
+        <Total>
+          <span>TOTAL</span>
+          <span>${total}</span>
+        </Total>
+      </>
     );
-
-    if (!confirmAlert) {
-      return;
-    }
-
-    try {
-      const { data } = await axios.delete(
-        `https://neon-game-store-back.herokuapp.com/cart/${_id}`,
-        // body,
-        config
-      );
-      console.log(data);
-      setProductsCart(data);
-    } catch (error) {
-      const message = error.response.statusText;
-      alert(message);
-    }
   }
 
-  async function SubmitCheckout(total) {
+  function RenderCustomerData() {
+    console.log(payment);
+    return (
+      <>
+        <input
+          type="name"
+          id="name"
+          value={name}
+          placeholder="Nome"
+          // onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          id="cpf"
+          value={cpf}
+          placeholder="CPF"
+          onChange={(e) => setCpf(e.target.value)}
+        />
+        <input
+          type="text"
+          id="phone"
+          value={phone}
+          placeholder="Telefone"
+          onChange={(e) => setPhone(e.target.value)}
+        />
+        <input
+          type="text"
+          id="address"
+          value={address}
+          required
+          placeholder="Endereço"
+          onChange={(e) => setAddress(e.target.value)}
+        />
+        <p>Forma de pagamento</p>
+        <RadioGroup>
+          <input
+            type="radio"
+            id="creditcard"
+            name="fav_language"
+            value="creditcard"
+            onChange={(e) => setPayment(e.target.value)}
+          />
+          <label htmlFor="creditcard">Cartão de Crédito</label>
+          <br></br>
+          <input
+            type="radio"
+            id="debitcard"
+            name="fav_language"
+            value="debitcard"
+            onChange={(e) => setPayment(e.target.value)}
+          />
+          <label htmlFor="debitcard">Cartão de Débito</label> <br></br>
+          <input
+            type="radio"
+            id="boleto"
+            name="fav_language"
+            value="boleto"
+            onChange={(e) => setPayment(e.target.value)}
+          />
+          <label htmlFor="boleto">Boleto</label>
+        </RadioGroup>
+      </>
+    );
+  }
+
+  async function SubmitOrder() {
     const config = {
       headers: {
         Authorization: `Bearer ${token}`,
@@ -131,7 +158,13 @@ export default function Checkout() {
     };
     console.log(total);
     const body = {
-      total: parseFloat(total),
+      name,
+      cpf,
+      products,
+      total,
+      address,
+      payment,
+      date
     };
 
     try {
@@ -140,12 +173,15 @@ export default function Checkout() {
         body,
         config
       );
-      navigate("/checkout");
+      DeleteCart();
+      navigate("/success");
     } catch (error) {
       const message = error.response.statusText;
       alert(message);
     }
   }
+
+  async function DeleteCart() {}
 
   return (
     <>
@@ -159,13 +195,11 @@ export default function Checkout() {
         <Collapsible trigger=">> Resumo do pedido">
           <Products>{RenderProducts()}</Products>
         </Collapsible>
-        <Collapsible trigger=">> Dados do comprador">
+        <Collapsible trigger=">> Dados da compra">
           <Products>{RenderCustomerData()}</Products>
+          {RenderTotal()}
         </Collapsible>
-        <Collapsible trigger=">> Condições de pagamento">
-          <Products>{RenderPayment()}</Products>
-        </Collapsible>
-        {RenderTotal()}
+        <button onClick={() => SubmitOrder()}>Finalizar compra</button>
       </Container>
       <Footer />
     </>
@@ -181,40 +215,29 @@ const Container = styled.main`
   overflow-x: hidden;
   justify-content: "flex-start";
   margin-top: 50px;
-  margin-bottom:110px;
+  margin-bottom: 50px;
   padding: 15px;
   overflow-y: hidden;
 
-  .Collapsible{
-     width:100%;
-     font-family: "Goldman", cursive;
-     font-size:20px;
-     margin-bottom: 30px;;
-     background-color: #212121;
-     border-radius: 10px;
-    color:#FFFFFF;
+  .Collapsible {
+    width: 100%;
+    font-family: "Goldman", cursive;
+    font-size: 18px;
+    margin-bottom: 30px;
+    background-color: #212121;
+    border-radius: 10px;
+    color: #d8d4d4;
     /* padding-right:5px; */
-    border: 0.2rem solid #fff;
-    border-radius: 1rem;
+    border: 0.1rem solid #fff;
+    border-radius: 0.1rem;
     padding: 0.4em;
-    box-shadow: 0 0 .2rem #fff,
-                0 0 .2rem #fff,
-                0 0 1.5rem #dfff1e,
-                0 0 0.8rem #dfff1e,
-                0 0 0.8rem #dfff1e,
-                inset 0 0 1.0rem #dfff1e; 
-    }
-
-  .Collapsible__contentInner{
-    height: calc((100vh - 380px));
+    box-shadow: 0 0 0.1rem #fff, 0 0 0.1rem #fff, 0 0 1rem #ffab2d,
+      0 0 0.4rem #ffab2d, 0 0 0.4rem #ffab2d, inset 0 0 0.8rem #ffab2d;
   }
 
-  p {
-    font-family: "Inria Sans", sans-serif;
-    color: #d8d4d4;
-    font-size: 24px;
-    text-align: center;
-    margin-bottom: 130px;
+  .Collapsible__contentInner {
+    /* max-height: calc((100vh - 380px)); */
+    max-height: max-content;
   }
 
   button {
@@ -228,11 +251,34 @@ const Container = styled.main`
     font-size: 18px;
     cursor: pointer;
   }
+
+  input {
+    /* height: 50px;
+    border-radius: 5px;
+    border-bottom: 1px; */
+    margin: 20px 0 20px 0;
+    font-family: "Inria Sans", sans-serif;
+    background-color: #212121;
+    outline: none;
+    border-width: 0 0 2px;
+    border-color: #ffab2d;
+    color: #ffffff;
+    font-size: 15px;
+  }
+  input:first-child {
+    margin-top: 40px;
+  }
+
+  p {
+    font-family: "Inria Sans", sans-serif;
+    margin-top: 20px;
+    font-size: 18px;
+  }
 `;
 
 const Header = styled.div`
   background-color: #151515;
-  color: #dfff1e;
+  color: #ffab2d;
   width: 100%;
   height: 50px;
   display: flex;
@@ -262,7 +308,7 @@ const Products = styled.div`
 `;
 
 const Product = styled.div`
-  color: #dfff1e;
+  color: #ffab2d;
   width: 100%;
   height: 160px;
   display: flex;
@@ -302,23 +348,31 @@ const Info = styled.div`
 `;
 
 const Total = styled.div`
-  color: #dfff1e;
-  background-color: #151515;
+  color: #ffab2d;
   width: 100%;
   display: flex;
   align-items: center;
   justify-content: space-between;
-
   padding: 15px;
 
   span {
-    color: #ffffff;
+    color: #d8d4d4;
     font-weight: 700;
     font-family: "Inria Sans", sans-serif;
     font-size: 18px;
   }
 
   span:nth-child(2) {
-    color: #dfff1e;
+    color: #ffab2d;
+  }
+`;
+
+const RadioGroup = styled.div`
+  font-family: "Inria Sans", sans-serif;
+  font-size: 16px;
+  margin-top: 20px;
+
+  input[type="radio"] {
+    margin: 0 3px 5px 10px;
   }
 `;
